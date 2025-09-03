302e90b589176c5318cc8411ab55de5585b489d8
Ujjawal Verma <ujjawalverma221@gmail.com>
2025-09-03 23:34:15 +0530

    changed few things

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import mongoose = require("mongoose");
+import { UserModel } from "./db";
 
 const app = express();
 
-app.post("api/v1/signup",(req,res)=>{
+app.post("api/v1/signup",async (req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+    //zod validaion and hash the password
+    await UserModel.create({
+        username:username,
+        password:password,
 
+    })
 })
 
 app.post("api/v1/signin",(req,res)=>{
 
 })
-
 
 app.post("api/v1/content",(req,res)=>{
 
@@ -30,5 +37,5 @@ app.post("api/v1/brain/share",(req,res)=>{
 })
 
 app.get("api/v1/brain:shareLink",(req,res)=>{
-    
+
 })
